6351c80490dfd38fd9f61d8b8601025dfaff7e14
gera <german_abramov@epam.com>
2019-06-30 20:44:15 +0300

    Стёпа

--- src/main/kotlin/com/terra/TerraApplication.kt
+++ src/main/kotlin/com/terra/TerraApplication.kt
@@ -1,16 +1,12 @@
 package com.terra
 
 import com.terra.apis.placeApi.amadeus.AmadeusPlaceApi
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
 class TerraApplication
 
 fun main(args: Array<String>) {
-	//SpringApplication.run(TerraApplication::class.java, *args)
-
-
-	val api = AmadeusPlaceApi()
-
-	api.places(41.397158, 2.160873, 1)
+	SpringApplication.run(TerraApplication::class.java, *args)
 }
---
+++ src/main/kotlin/com/terra/dto/PlaceDto.kt
@@ -0,0 +1,4 @@
+package com.terra.dto
+
+class PlaceDto {
+}
